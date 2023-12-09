b2cd3fc7cd8ab9bae1c671f696e61e2a3fcfa13e
Froillan Kim B. Edem <112741319+Froillan123@users.noreply.github.com>
2023-12-09 14:55:38 +0800

    ATAY KA

--- js/script.js
+++ js/script.js
@@ -1,67 +1,143 @@
 let queue = [];
 let rear = -1;
-let size = 5; 
+let size = 5;
+let selectedItems = [];
 
 function addCustomer() {
-    let newCustomer = prompt("Enter new customer name:");
+    let newCustomerName = document.querySelector('.box1').value;
+    selectedItems = getSelectedItems();
 
-    if (isQueueFull()) {
-        alert("Queue is full!");
+    if (newCustomerName.trim() === '') {
+        alert('Please enter your name!');
         return;
     }
 
+    if (selectedItems.length === 0) {
+        alert('Please select at least one item!');
+        return;
+    }
+
+    if (isQueueFull()) {
+        alert('Queue is full!');
+        return;
+    }
 
     rear = (rear + 1) % size;
-    queue[rear] = newCustomer;
+    queue[rear] = { name: newCustomerName, items: [...selectedItems] };
     displayQueue();
+    resetCart();
+    resetForm();
+    // Optionally, close the form after adding the customer
+    document.querySelector('.login-form').classList.remove('active');
 }
 
-function getRandomRefNo() {
-    let random3Digit = Math.floor(Math.random() * 900) + 100;
-    return "1772" + random3Digit;
+function getSelectedItems() {
+    let items = document.querySelectorAll('.Items input[type=checkbox]:checked + span');
+    return Array.from(items).map(item => item.textContent);
 }
 
 function isQueueFull() {
-    return rear == size - 1;
+    return rear === size - 1;
 }
 
 function dequeueCustomer() {
     if (isEmpty()) {
-        alert("Queue is empty!");
-        return;
-    }
-
-    if (rear < 0) {
-        alert("Queue is empty!");
+        Swal.fire({
+            icon: 'warning',
+            title: 'Queue is empty!',
+        });
         return;
     }
 
     let dequeuedCustomer = queue.shift();
-    rear = rear - 1;
+    rear--;
 
     if (isEmpty()) {
         rear = -1;
     }
 
     displayQueue();
+    resetForm(); 
 }
 
 function isEmpty() {
-    return rear == -1;
+    return rear === -1;
 }
-
-
 
 function displayQueue() {
     for (let i = 0; i < size; i++) {
         let customer = queue[i];
+        let customerInfoElement = document.getElementById("customerInfo" + (i + 1));
+        let goodsListElement = document.getElementById("goodsList" + (i + 1));
 
         if (customer) {
-            document.getElementById("customerInfo" + (i + 1)).textContent = customer;
-            document.getElementById("goodsList" + (i + 1)).textContent = "Item 1, Item 2, Item 3";
+            customerInfoElement.textContent = customer.name;
+
+            if (document.querySelector('.login-form').classList.contains('active')) {
+
+                if (customer.items.length > 0) {
+                    goodsListElement.innerHTML = "Item Ordered:<br>";
+                    goodsListElement.innerHTML += "<ol>"; // Start ordered list
+
+                    customer.items.forEach(item => {
+                        goodsListElement.innerHTML += `<li>${item}</li>`;
+                    });
+
+                    goodsListElement.innerHTML += "</ol>"; // End ordered list
+                } else {
+                    goodsListElement.innerHTML = "Item Ordered:"; // Display only when items are present
+                }
+            }
         } else {
-            document.getElementById("customerInfo" + (i + 1)).textContent = "";
-            document.getElementById("goodsList" + (i + 1)).textContent = "";
+            customerInfoElement.textContent = "";
+            goodsListElement.innerHTML = "Item Ordered:"; // Reset content when no customer
         }
     }
 }
+
+
+
+function resetCart() {
+    selectedItems = [];
+    document.querySelector('.Cart').textContent = 'Your Ordered Items:';
+}
+
+function resetForm() {
+    document.getElementById('form').reset();
+}
+
+document.querySelectorAll('.Items input[type=checkbox]').forEach(checkbox => {
+    checkbox.addEventListener('change', () => {
+        updateCart();
+    });
+});
+
+
+function updateCart() {
+    selectedItems = getSelectedItems();
+    let cartElement = document.querySelector('.Cart');
+
+    if (selectedItems.length > 0) {
+        cartElement.innerHTML = 'Your Ordered Items:<br>';
+        selectedItems.forEach(item => {
+            cartElement.innerHTML += `${item}<br>`;
+        });
+    } else {
+        cartElement.innerHTML = 'Your Ordered Items:';
+    }
+}
+
+let loginForm = document.querySelector('.login-form');
+
+document.querySelector('#login-btn').onclick = () => {
+    loginForm.classList.toggle('active');
+}
+
+document.querySelector('#close-login-btn').onclick = () => {
+    loginForm.classList.remove('active');
+}
+
+document.getElementById('form').addEventListener('submit', function (event) {
+    event.preventDefault();
+    addCustomer();
+});
